ab40c14754dd0fc74d4b302c44bf8acd4e08ee85
[Spencer Cook] <spencerrcook@hotmail.com>
2023-11-26 12:00:04 -0700

    add arrays for various password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// variables for password criteria
+var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var specialCharactersArray = specialCharacters.split("");
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var lowercaseCharactersArray = lowercaseCharacters.split("");
+var uppercaseCharactersArray = lowercaseCharacters.toUpperCase().split("");
+var numericArray = "0123456789".split("");
+
 // This is called in writePassword(), just needs to generate the password
 function generatePassword() {
+  var validcharacters = [];
+  var generatedPassword = "";
+
   var userCriteria = {
     length: prompt("What is the desired password length? (8-128 characters)"), // int
     lowercase: confirm("Include lowercase characters?"), // bool
@@ -12,6 +23,24 @@ function generatePassword() {
     numeric: confirm("Include numeric characters?"), // bool
     special: confirm("Include special characters?"), // bool
   };
+
+  if (userCriteria.lowercase === true) {
+    validcharacters = validcharacters.concat(lowercaseCharactersArray);
+  }
+  if (userCriteria.uppercase === true) {
+    validcharacters = validcharacters.concat(uppercaseCharactersArray);
+  }
+  if (userCriteria.numeric === true) {
+    validcharacters = validcharacters.concat(numericArray);
+  }
+  if (userCriteria.special === true) {
+    validcharacters = validcharacters.concat(specialCharactersArray);
+  }
+
+  //main loop for random password
+  for (var i = 0; i < userCriteria.length; i++) {
+    // generatedPassword = generatedPassword +
+  }
 }
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Password Generator Starter Code
 
 starter files from https://github.com/coding-boot-camp/friendly-parakeet
+password special characters from https://owasp.org/www-community/password-special-characters
